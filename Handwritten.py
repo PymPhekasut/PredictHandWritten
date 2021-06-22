3ea362e9e3a74ece5e2c98dd0dd1272fbf1fd9c2
PymPhekasut <74609915+PymPhekasut@users.noreply.github.com>
2021-06-22 20:31:13 +1000

    Create Handwritten.py

---
+++ Handwritten.py
@@ -0,0 +1,53 @@
+# Recognizing hand-written digits
+import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+digits = datasets.load_digits()
+dir(digits)
+
+#Each slot in the array corresponds to a pixel
+#The value in the slot is the amount of black in the pixel
+print (digits.images[0])
+
+
+"""
+[[ 0.  0.  5. 13.  9.  1.  0.  0.]
+ [ 0.  0. 13. 15. 10. 15.  5.  0.]
+ [ 0.  3. 15.  2.  0. 11.  8.  0.]
+ [ 0.  4. 12.  0.  0.  8.  8.  0.]
+ [ 0.  5.  8.  0.  0.  9.  8.  0.]
+ [ 0.  4. 11.  0.  1. 12.  7.  0.]
+ [ 0.  2. 14.  5. 10. 12.  0.  0.]
+ [ 0.  0.  6. 13. 10.  0.  0.  0.]]
+ """
+
+#Look for more images 
+
+def plot_multi(i):
+    '''Plots 16 digits, starting with digit i'''
+    nplots = 16
+    fig = plt.figure(figsize=(15,15))
+    for j in range(nplots):
+        plt.subplot(4,4,j+1)
+        plt.imshow(digits.images[i+j], cmap='binary')
+        plt.title(digits.target[i+j])
+        plt.axis('off')
+    plt.show()
+    
+    plot_multi(0)
+#you can have a look at the next digits by calling plot_multi(16), plot_multi(32)
+
+
+# Python code for digits classification
+
+digits = datasets.load_digits()
+
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for ax, image, prediction in zip(axes, X_test, predicted):
+    ax.set_axis_off()
+    image = image.reshape(8, 8)
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    ax.set_title(f'Prediction: {prediction}')
